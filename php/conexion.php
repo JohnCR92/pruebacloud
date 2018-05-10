58462e1e9abb6e4d993aa528352290f055f96b2b
JohnCR92 <johncuasapud@gmail.com>
2018-05-09 21:15:36 -0500

    comit2

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,16 @@
 <?php
-$host="10.129.180.91";
-$user="johnc@servidorjohn";
-$password="JohnHCR92";
-$db="servidorbdd";
-$con = new mysqli($host,$user,$password,$db);
+try {
+    $conn = new PDO("sqlsrv:server = tcp:servidorjohn.database.windows.net,1433; Database = servidorbdd", "johnc", "JohnHCR92");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "johnc@servidorjohn", "pwd" => "JohnHCR92", "Database" => "servidorbdd", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:servidorjohn.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 ?>
