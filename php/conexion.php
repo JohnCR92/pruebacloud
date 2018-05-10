988739d9d60a51d111341b5306c0d52010c61ad7
JohnCR92 <johncuasapud@gmail.com>
2018-05-09 23:54:03 -0500

    comit4

--- php/conexion.php
+++ php/conexion.php
@@ -1,16 +1,54 @@
 <?php
-try {
-    $conn = new PDO("sqlsrv:server = tcp:servidorjohn.database.windows.net,1433; Database = servidorbdd", "johnc", "{JohnHCR92}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+/**
+ * Clase utilitaria que maneja la conexion/desconexion a la base de datos
+ * mediante las funciones PDO (PHP Data Objects).
+ * Utiliza el patron de diseno singleton para el manejo de la conexion.
+ * @author 
+ */
+class Database {
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "johnc@servidorjohn", "pwd" => "{JohnHCR92}", "Database" => "servidorbdd", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:servidorjohn.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+    //Propiedades estaticas con la informacion de la conexion (DSN):
+    private static $dbName = 'servidorbdd';
+    private static $dbHost = 'servidorjohn.database.windows.net';
+    private static $dbUsername = 'johnc';
+    private static $dbUserPassword = 'JohnHCR92';
+    //Propiedad para control de la conexion:
+    private static $conexion = null;
+
+    /**
+     * No se permite instanciar esta clase, se utilizan sus elementos
+     * de tipo estatico.
+     */
+    public function __construct() {
+        exit('No se permite instanciar esta clase.');
+    }
+
+    /**
+     * Metodo estatico que crea una conexion a la base de datos.
+     * @return type
+     */
+    public static function connect() {
+        // Una sola conexion para toda la aplicacion (singleton):
+        if (null == self::$conexion) {
+            try {
+                //self::$conexion = new PDO("mysql:host=" . self::$dbHost . ";" . "dbname=" . self::$dbName, self::$dbUsername, self::$dbUserPassword);
+                self::$conexion = new PDO("tcp:servidorjohn.database.windows.net,1433; Database = servidorbdd", "johnc", "JohnHCR92");
+                self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                print("Error connecting to SQL Server.");
+                die($e->getMessage());
+            }
+        }
+        return self::$conexion;
+    }
+
+    /**
+     * Metodo estatico para desconexion de la bdd.
+     */
+    public static function disconnect() {
+        self::$conexion = null;
+    }
+
+}
 
 ?>
