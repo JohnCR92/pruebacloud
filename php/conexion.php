0157deadea839a52ee9ffc308541125e82dbdf16
JohnCR92 <johncuasapud@gmail.com>
2018-05-09 21:32:14 -0500

    commit3

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO("sqlsrv:server = tcp:servidorjohn.database.windows.net,1433; Database = servidorbdd", "johnc", "JohnHCR92");
+    $conn = new PDO("sqlsrv:server = tcp:servidorjohn.database.windows.net,1433; Database = servidorbdd", "johnc", "{JohnHCR92}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -9,7 +9,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "johnc@servidorjohn", "pwd" => "JohnHCR92", "Database" => "servidorbdd", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "johnc@servidorjohn", "pwd" => "{JohnHCR92}", "Database" => "servidorbdd", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:servidorjohn.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
